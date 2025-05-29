98c2e62ab3aa71724b4422e430fcf57b33ec3d57
Nyfo <Nyfoun@gmail.com>
2025-05-29 14:03:48 +0200

    ???

--- Kode/QUAD.py
+++ Kode/QUAD.py
@@ -1,13 +1,13 @@
 from scipy.integrate import quad
-from scipy.stats import beta, uniform  # Du kan også importere andre fordelinger
+from scipy.stats import beta, uniform, gamma  # Du kan også importere andre fordelinger
 
 # Eksempel: F_cdf baseret på en Beta(3,2)-fordeling
-F_cdf = lambda v: beta.cdf(v, 3, 2)
+F_cdf_beta = lambda v: beta.cdf(v, 3, 2)
 
 # Antal observationer (skal defineres før kald af b)
 n = 5  # eller hvad din n skal være
 
-def b(v):
+def b(v, F_cdf=F_cdf_beta, n=n):
     """
     Beregner b(v) = v - ( ∫₀ᵛ [F_cdf(x)]^(n-1) dx ) / [F_cdf(v)]^(n-1)
 
@@ -17,6 +17,7 @@ def b(v):
     Returnerer:
         float: Værdien af b(v).
     """
+
     # Numerator: integral af F_cdf(x)^(n-1) fra x=0 til x=v
     numer = quad(lambda x: F_cdf(x)**(n-1), 0, v)[0]
 
@@ -24,8 +25,3 @@ def b(v):
     denom = F_cdf(v)**(n-1)
 
     return v - numer/denom
-
-# Eksempel på brug
-if __name__ == "__main__":
-    for v_test in [0.2, 0.5, 0.8]:
-        print(f"b({v_test}) = {b(v_test):.6f}")
